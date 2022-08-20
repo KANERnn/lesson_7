2fe780896df7956d1198227ae2771902cc077ffd
KANERnn <kanerkanatbekov@gmail.com>
2022-08-20 22:51:41 +0600

    dodelal

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Medic medic = new Medic(150, 50, 50);
         System.out.println(medic.getDamage() + " " + medic.getHp());
-        medic.applySuperAbility();
         Magic magic = new Magic(200, 40);
         System.out.println(magic.getDamage() + " " + magic.getHp());
-        magic.applySuperAbility();
         Warrior warrior = new Warrior(100, 30);
         System.out.println(warrior.getDamage() + " " + warrior.getHp());
-        warrior.applySuperAbility();
         Hero[] hero = {
                 medic, warrior, magic
         };
         for (int i = 0; i < hero.length; i++) {
+            hero[i].applySuperAbility();
             if (hero[i] == medic) {
                 System.out.println(medic.increaseExperience());
             }
